4bcf3232ccafcd1e6cac25b03b9c3cdf7b10294d
Rik Schennink <rikschennink@gmail.com>
2018-01-11 10:51:44 +0100

    reformat componet.js, fix import mode, switch to .esm file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-import * as conditioner from 'conditioner-core';
+import * as conditioner from 'conditioner-core/conditioner-core.esm';
 
 conditioner.addPlugin({
-
     // converts module aliases to paths
-    moduleSetName: (name) => `./ui/${ name }.js`,
-    
+    moduleSetName: name => `./ui/${name}.js`,
+
     // use default exports as constructor
-    moduleGetConstructor: (module) => module.default,
+    moduleGetConstructor: module => module.default,
 
     // override the import (this makes webpack bundle all the dynamically included files as well)
-    moduleImport: (name) => import(
-        /* https://webpack.js.org/api/module-methods/#import- */
-        /* set to "eager" to create a single chunk for all modules */
-        /* set to "lazy" to create a separate chunk for each module */
-        /* webpackMode: "lazy" */
-        `${ name }`
-    )
-
+    // https://webpack.js.org/api/module-methods/#import-
+    // - set to "eager" to create a single chunk for all modules
+    // - set to "lazy" to create a separate chunk for each module
+    moduleImport: name => import(/* webpackMode: "lazy" */ `${name}`)
 });
 
 // lets go!
-conditioner.hydrate( document.documentElement );
+conditioner.hydrate(document.documentElement);
